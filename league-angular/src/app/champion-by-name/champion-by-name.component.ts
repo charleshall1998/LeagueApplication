16093e339fcff116726579ea4866645fa233fd8e
Charles Hall <charleshall@C02DFWLKML7H.attlocal.net>
2021-03-11 17:27:33 -0500

    Dynamically generated html elements and select for champion by name.

--- league-angular/src/app/champion-by-name/champion-by-name.component.ts
+++ league-angular/src/app/champion-by-name/champion-by-name.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { Champion } from '../Champion';
 import { LeagueService } from '../league.service';
 
@@ -12,19 +12,30 @@ export class ChampionByNameComponent implements OnInit {
   champion : Champion;
   championName : string = " ";
   src : string;
+  championNameContainer : string;
+  championInfoContainer : string;
+  championsList : Champion[];
 
   constructor(private service : LeagueService) { }
 
   ngOnInit(): void {
-
+    this.service.getAllChampions().subscribe(list => { 
+      this.championsList = list;
+    });
   }
 
   search(): void {
     this.championName = (document.getElementById("championName") as HTMLInputElement).value;
+    this.championNameContainer = '<h1>'+this.championName+'</h1>';
 
     this.service.getChampionByName(this.championName).subscribe( champ => {
       this.champion = champ;
       this.src = "./assets/images/splash/"+this.championName+".jpg";
+      this.championInfoContainer = '<p>'+this.champion.championDescription+'</p>'
+      +'<p> KDA: '+this.champion.avgKDA+'</p>'
+      +'<p>Win Rate: '+this.champion.winRate+'%</p>'
+      +'<p>Pick Rate: '+this.champion.pickRate+'%</p>'
+      +'<p>Ban Rate: '+this.champion.banRate+'%</p>';
     });
   }
 
